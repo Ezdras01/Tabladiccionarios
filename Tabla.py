e0be802bfcf1d02134a12f17198e31659833896e
Ezdras <132713544+Ezdras01@users.noreply.github.com>
2024-03-25 12:04:55 -0600

    Corrección de Ortografía
    Se pusieron acentos a dichas palabras

--- Tabla.py
+++ Tabla.py
@@ -4,6 +4,6 @@ from tabulate import tabulate
 print("Tabla de Traducciones\n")
 for i in range(1,4):
     if i == 1:
-        print(tabulate([[i,Diccionario.dics['esp'][str(i)],Diccionario.dics['deutsch'][str(i)],Diccionario.dics['francais'][str(i)],Diccionario.dics['english'][str(i)]]],headers=["Numeros","Español","Alemán","Frances","Ingles"]))
+        print(tabulate([[i,Diccionario.dics['esp'][str(i)],Diccionario.dics['deutsch'][str(i)],Diccionario.dics['francais'][str(i)],Diccionario.dics['english'][str(i)]]],headers=["Números","Español","Alemán","Francés","Inglés"]))
     else:
         print(tabulate([[i,Diccionario.dics['esp'][str(i)],Diccionario.dics['deutsch'][str(i)],Diccionario.dics['francais'][str(i)],Diccionario.dics['english'][str(i)]]],headers=["       ","       ","      ","       ","      "]))
