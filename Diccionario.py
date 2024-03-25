7ae7ae5cfb0ec0bdb6223523598d977fe44b387e
Ezdras <132713544+Ezdras01@users.noreply.github.com>
2024-03-25 11:08:36 -0600

    Diccionario
    Se agrego el diccionario muestra para poder realizar la tabla

---
+++ Diccionario.py
@@ -0,0 +1,4 @@
+dics= {'esp':{'1':'uno','2':'dos','3':'tres'}, 
+    'deutsch':{'1':'eins','2':'zwei','3':'drei'},     
+    'francais':{'1':'un','2':'deux','3':'trois'},
+    'english':{'1':'one','2':'two','3':'three'}}
