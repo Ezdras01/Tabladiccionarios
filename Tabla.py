699db5d113aedfe4ab78f86de93d2377abf348d7
Ezdras <132713544+Ezdras01@users.noreply.github.com>
2024-03-25 12:03:24 -0600

    Funciones
    Se realizaron funciones necesarias para imprimir de manera correcta la tabla

--- Tabla.py
+++ Tabla.py
@@ -1,2 +1,9 @@
 import Diccionario
-print("Diccionario: ", Diccionario.dics)
+from tabulate import tabulate
+#print(tabulate(headers=["Numeros","Español","Alemán","Frances","Ingles"]))
+print("Tabla de Traducciones\n")
+for i in range(1,4):
+    if i == 1:
+        print(tabulate([[i,Diccionario.dics['esp'][str(i)],Diccionario.dics['deutsch'][str(i)],Diccionario.dics['francais'][str(i)],Diccionario.dics['english'][str(i)]]],headers=["Numeros","Español","Alemán","Frances","Ingles"]))
+    else:
+        print(tabulate([[i,Diccionario.dics['esp'][str(i)],Diccionario.dics['deutsch'][str(i)],Diccionario.dics['francais'][str(i)],Diccionario.dics['english'][str(i)]]],headers=["       ","       ","      ","       ","      "]))
